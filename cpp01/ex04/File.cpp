31388eaef5577eed970229ab78f56874f04a6717
Wei Jian Hoe <wjhoe@c2r6s8.42singapore.sg>
2025-08-23 17:30:01 +0800

    Makefile done for ex02

--- cpp01/ex04/File.cpp
+++ cpp01/ex04/File.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   File.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wjhoe <wjhoe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/20 23:34:59 by weijian           #+#    #+#             */
-/*   Updated: 2025/08/23 14:55:40 by wjhoe            ###   ########.fr       */
+/*   Created: 2025/08/23 15:21:30 by wjhoe             #+#    #+#             */
+/*   Updated: 2025/08/23 15:21:31 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-#include <cstdlib>
-
-Zombie* zombieHorde( int N, std::string name );
-
-int main(int ac, char **av)
-{
-	if (ac < 2)
-		return (0);
-	std::string	name;
-	if (ac > 2)
-		name = av[2];
-	else {
-		std::cout << "name please: ";
-		std::cin >> name;
-	}
-	Zombie* horde = zombieHorde(std::atoi(av[1]), name);
-	delete[] horde;
-}
--- cpp01/ex04/File.hpp
+++ cpp01/ex04/File.hpp
@@ -1,31 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   File.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wjhoe <wjhoe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/20 23:34:59 by weijian           #+#    #+#             */
-/*   Updated: 2025/08/23 14:55:40 by wjhoe            ###   ########.fr       */
+/*   Created: 2025/08/23 15:21:28 by wjhoe             #+#    #+#             */
+/*   Updated: 2025/08/23 15:24:29 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-#include <cstdlib>
+#pragma once
 
-Zombie* zombieHorde( int N, std::string name );
+#include <iostream>
 
-int main(int ac, char **av)
+class File
 {
-	if (ac < 2)
-		return (0);
-	std::string	name;
-	if (ac > 2)
-		name = av[2];
-	else {
-		std::cout << "name please: ";
-		std::cin >> name;
-	}
-	Zombie* horde = zombieHorde(std::atoi(av[1]), name);
-	delete[] horde;
+	public:
+
+	private:
+	/* variables */
+		
 }
