2f10e71c37126437f8547a3f9c45ea626a9b562e
wweijian <hoe.weijian@gmail.com>
2025-08-24 17:33:11 +0800

    cpp02

---
+++ cpp02/ex00/ex00/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijian <weijian@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 17:08:01 by weijian           #+#    #+#             */
+/*   Updated: 2025/08/24 17:32:41 by weijian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class Fixed 
+{
+	public:
+	/* variables */
+	/* functions */
+	private:
+	/* variables */
+	int					integer;
+	static const int	fractionalBits;
+	/* functions */
+	
+} ;
