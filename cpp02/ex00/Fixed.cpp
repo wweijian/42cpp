258ee66523c7bf59d6becf83c006a3e8b68cfa00
wweijian <hoe.weijian@gmail.com>
2025-08-24 18:19:42 +0800

    wip cpp02

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,8 +6,28 @@
 /*   By: weijian <weijian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 17:08:05 by weijian           #+#    #+#             */
-/*   Updated: 2025/08/24 17:32:57 by weijian          ###   ########.fr       */
+/*   Updated: 2025/08/24 18:17:38 by weijian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+/* constructors */
+
+Fixed::Fixed()
+	:	_integer(0)
+{}
+
+Fixed::~Fixed()
+{}
+
+Fixed::Fixed(const Fixed &fixed)
+	:	_integer(fixed.getRawBits)
+{}
+
+Fixed& Fixed::operator=(const Fixed &fixed);
+
+
+/* static constants */
+const int	Fixed::_fractionalBits = 8;
+
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: weijian <weijian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 17:08:01 by weijian           #+#    #+#             */
-/*   Updated: 2025/08/24 17:32:41 by weijian          ###   ########.fr       */
+/*   Updated: 2025/08/24 18:11:48 by weijian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,32 @@
 
 #include <iostream>
 
+
+/* 
+	The const after the parentheses applies to the member function itself, 
+	meaning that the function does not modify the object it belongs to
+	Inside this function, you cannot change any non-mutable member 
+	variables of the class.
+
+	[const int function] just means you can't modify the output later
+*/
 class Fixed 
 {
 	public:
-	/* variables */
+	/* constructors */
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &fixed);
+	Fixed& operator=(const Fixed &fixed);
+	
 	/* functions */
+	int		getRawBits() const;
+	void	setRawBits(int const raw);
+	
 	private:
 	/* variables */
-	int					integer;
-	static const int	fractionalBits;
+	int					_integer;
+	static const int	_fractionalBits;
 	/* functions */
 	
 } ;
